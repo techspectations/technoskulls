57196a6871303fc419285fff301ed48a642964e3
krishnaraj9 <krishnaraj9@users.noreply.github.com>
2016-11-12 12:52:39 +0530

    Add files via upload

---
+++ CameraShake.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+    public GameObject goCam;
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            goCam = gameObject;
+            camTransform = gameObject.transform ;
+
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.position;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+}
